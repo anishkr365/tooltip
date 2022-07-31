98a2ae12aac2aea3156ed755bed01c28fa7592f0
Anish <anish.kr365@gmail.com>
2022-07-31 23:46:11 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+// import firebase from "firebase/app";
+// import "firebase/firestore";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBXcp__bxkoukRA1yF2_0RSR_gfDPel-Ws",
+//     authDomain: "react-hooks-blog-297bc.firebaseapp.com",
+//     projectId: "react-hooks-blog-297bc",
+//     storageBucket: "react-hooks-blog-297bc.appspot.com",
+//     messagingSenderId: "706395739782",
+//     appId: "1:706395739782:web:5e0e2b354bad7fd699a307"
+//   };
+  
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
+
+ReactDOM.render(<App/>, document.getElementById('root'));
